12a0dabb38b9a5b3034e955606044d1cdccaeba6
angelitto2005 <angelitto2005@gmail.com>
2026-05-02 16:57:03 +0300

    Update MCB

--- all/script.kodi.mybackup/default.py
+++ all/script.kodi.mybackup/default.py
@@ -89,17 +89,21 @@ def download_from_vfs(src_vfs, dest_local, dialog, start_pct, end_pct):
         copied = 0
         chunk_size = 4 * 1024 * 1024 # 4 MB chunks
 
-        f_in = xbmcvfs.File(src_vfs, 'r')
+        # ATENTIE: Am scos 'r' ca sa nu se mai blocheze in UTF-8
+        f_in = xbmcvfs.File(src_vfs)
         try:
             with open(dest_local, 'wb') as f_out:
                 while True:
                     if dialog.iscanceled():
                         return False
-                    chunk = f_in.read(chunk_size)
+                    
+                    # ATENTIE: Folosim readBytes in loc de read pt fisiere ZIP
+                    chunk = f_in.readBytes(chunk_size)
                     if not chunk:
                         break
                     
-                    f_out.write(chunk)
+                    # Scriem array-ul de byti
+                    f_out.write(bytearray(chunk))
                     copied += len(chunk)
                     
                     if total_size > 0:
